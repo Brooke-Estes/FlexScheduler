c127bb569fa7840d41c6c2ed2f61f4507ae25970
Brooke-Estes <brooke@estes-family.net>
2016-01-12 22:05:35 -0600

    Adding StoreHours and Availaibility Class Maps

---
+++ src/FlexScheduler.Domain/ClassMaps/AvailaibilityClassMap.cs
@@ -0,0 +1,14 @@
+﻿namespace FlexScheduler.Domain.ClassMaps
+{
+    using Entities;
+    using FluentNHibernate.Mapping;
+
+    public class AvailaibilityClassMap : SubclassMap<Availaibility>
+    {
+        public AvailaibilityClassMap()
+        {
+            Map(x => x.FullName).Column("Name");
+            DiscriminatorValue("Availability");
+        }
+    }
+}
---
+++ src/FlexScheduler.Domain/ClassMaps/StoreHoursClassMap.cs
@@ -0,0 +1,14 @@
+﻿namespace FlexScheduler.Domain.ClassMaps
+{
+    using Entities;
+    using FluentNHibernate.Mapping;
+
+    public class StoreHoursClassMap : SubclassMap<StoreHours>
+    {
+        public StoreHoursClassMap()
+        {
+            Map(x => x.Name);
+            DiscriminatorValue("Store");
+        }
+    }
+}
