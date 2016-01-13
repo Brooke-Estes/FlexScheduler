7148d6b71b8bbc83187b2d16fa3d22ff39c301c3
Brooke-Estes <brooke@estes-family.net>
2016-01-12 23:24:36 -0600

    Updated persistence tests.

---
+++ tests/FlexScheduler.Integration/Persistence.cs
@@ -0,0 +1,42 @@
+﻿namespace FlexScheduler.Integration
+{
+    using Entities;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Persistence : SessionBase
+    {
+        private const string City = "Topeka";
+        private const string State = "KS";
+
+        [TestCase( "Westridge Kiosk", "1801 SW Wanamaker Rd", "66604" )]
+        [TestCase( "Hunters Ridge", "4731 NW Hunters Ridge Cir.", "66618" )]
+        public void CanCreateStore(string name, string street, string zip)
+        {
+            using (var session = _sessionFactory.OpenSession())
+            {
+                using (var trx = session.BeginTransaction())
+                {
+                    var store = new Store
+                    {
+                        Name = name,
+                        Street = street,
+                        City = City,
+                        State = State,
+                        Zip = zip
+                    };
+
+                    session.Save(store);
+                    trx.Commit();
+                    Assert.AreNotEqual(store.Id,0);
+                }
+            }
+        }
+
+        [TestCase()]
+        public void CanCreateStoreHours()
+        {
+            
+        }
+    }
+}
---
+++ tests/FlexScheduler.Integration/SessionBase.cs
@@ -0,0 +1,37 @@
+﻿namespace FlexScheduler.Integration
+{
+    using System.Configuration;
+    using Domain.ClassMaps;
+    using Entities;
+    using FluentNHibernate.Cfg;
+    using FluentNHibernate.Cfg.Db;
+    using NHibernate;
+    using NUnit.Framework;
+    using NUnit.Framework.Internal;
+
+    [TestFixture]
+    public abstract class SessionBase
+    {
+        public ISessionFactory _sessionFactory;
+
+        [SetUp]
+        public void SetContext()
+        {
+            var cnx = ConfigurationManager.ConnectionStrings["FlexSchedulerHome"].ConnectionString;
+
+            _sessionFactory = Fluently.Configure()
+                .Database(MsSqlConfiguration.MsSql2012
+                    .ConnectionString(cnx)
+                    .ShowSql())
+                .Mappings(m => m.FluentMappings.AddFromAssemblyOf<StoreClassMap>())
+                .BuildSessionFactory();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (_sessionFactory != null) 
+                _sessionFactory.Close();
+        }
+    }
+}
